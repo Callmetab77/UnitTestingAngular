a39b3e9ec26664e17887a1a0d3e06c2629fe8f05
Michael John Gatab <michaeljohn.gatab@asti.dost.gov.ph>
2023-03-31 06:40:08 +0800

    sample of shalow testing

---
+++ src/app/hero/hero.component.shallow.spec.ts
@@ -0,0 +1,28 @@
+import { NO_ERRORS_SCHEMA } from "@angular/core";
+import { ComponentFixture, TestBed } from "@angular/core/testing"
+import { HeroComponent } from "./hero.component"
+
+describe('HeroComponent (shallow tests)', ()=>{
+
+  let fixture: ComponentFixture<HeroComponent>;
+  beforeEach(() => {
+    //TestBed allow you to test both UI and Component
+    TestBed.configureTestingModule({
+      declarations: [HeroComponent],
+      schemas: [NO_ERRORS_SCHEMA]
+    });
+    //NO_ERRORS_SCHEMA will make the testing don't validate the template
+    fixture = TestBed.createComponent(HeroComponent);
+  })
+
+  it('should have the correct hero', () => {
+    fixture.componentInstance.hero = {
+      id: 1,
+      name: 'superDude',
+      strength: 3
+    }
+
+    //sample to detect changes fixture.detectChanges();
+    expect(fixture.componentInstance.hero.name).toEqual('superDude')
+  })
+})
